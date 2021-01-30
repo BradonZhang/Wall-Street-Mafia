6c7dd1cacf5fdcf4fb926566d1bbdf0c4f996209
Rohil Tuli <rohil.tuli@gmail.com>
2021-01-30 02:22:42 -0500

    Fixed issue with terminal.css yarn install

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -2,13 +2,29 @@ import 'terminal.css';
 
 function Home() {
     return (
-        <div className="components components-grid">
-            <aside style={{display: 'block'}}>
-                asdf
-            </aside>
-            <main>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div style={{flexGrow: 2}}>
+                <header>
+                    <h2>current prices</h2>
+                </header>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>symbol</th>
+                            <th>price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div style={{flexGrow: 1}} />
+            <div style={{flexGrow: 6}}>
                 asdfasdf
-            </main>
+            </div>
         </div>
     )
 }
